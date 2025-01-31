c8b1a800591fa49aa85573238d282c7c34380896
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-01-31 18:07:39 +1300

    Added basic solarized-dark styling.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,6 @@ import { ref, onMounted } from 'vue'
 import { useHome } from '@/composables/useHome'
 import type { Home } from '@/types/home'
 
-/**
-  While the data is cold, the composable is the correct choice for a unified
-  interface for the hotter data in the logs (at the cost of an extra
-  abstraction, but this is fine for the scope of the application).
- */
-
 const { fetchHome } = useHome()
 const homeData = ref<Home>({ favourites: [], channels: [] })
 
@@ -26,7 +20,7 @@ onMounted(async () => {
     <h1 class="page-heading">Home</h1>
 
     <section v-if="homeData.favourites.length" class="content-section">
-      <h2 class="section-heading">Favorites</h2>
+      <h3 class="section-heading">Favorites</h3>
       <ul class="item-list">
         <li v-for="favourite in homeData.favourites" :key="favourite" class="list-entry">
           <a :href="`/logs/${favourite}`">{{ favourite.replace('++', '##') }}</a>
@@ -35,7 +29,7 @@ onMounted(async () => {
     </section>
 
     <section class="content-section">
-      <h2 class="section-heading">Channels</h2>
+      <h3 class="section-heading">Channels</h3>
       <ul class="item-list">
         <li v-for="channel in homeData.channels" :key="channel" class="list-entry">
           <a :href="`/logs/${channel}`">{{ channel.replace('++', '##') }}</a>
