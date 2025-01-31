c8fd05027c26baeec63bfb5403079069feb9bec2
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-01-31 17:01:41 +1300

    Added Channel and Log views.

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/logs/:channel',
+      name: 'channel',
+      component: () => import('../views/ChannelView.vue'),
+    },
+    {
+      path: '/logs/:channel/:date',
+      name: 'log',
+      component: () => import('../views/LogView.vue'),
     },
   ],
 })
--- src/views/ChannelView.vue
+++ src/views/ChannelView.vue
@@ -1,5 +1,5 @@
 <template>
   <main>
-    <h1>About</h1>
+    <h1>Channel</h1>
   </main>
 </template>
